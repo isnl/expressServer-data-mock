41b7c312a9c026a599be60ecfe6e6ebb321da4f4
candy <htmlcs@163.com>
2020-02-19 17:54:21 +0800

    dy下载链接提供接口转发

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
+var express = require("express");
+const request = require("request");
 var router = express.Router();
 
 //跨域请求头   *为允许所有域名  上线请自行修改
-router.all("*", function (req, res, next) {
+router.all("*", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
@@ -13,11 +14,32 @@ router.all("*", function (req, res, next) {
 });
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get("/", function(req, res, next) {
   // res.render('index', { title: 'Express' });
   res.json({
     name: "candy"
-  })
+  });
+});
+router.get("/video", (req, res, next) => {
+  const queryUrl = req.query.url;
+  try {
+    request
+      .get({
+        url: queryUrl,
+        gzip: true,
+        headers: {
+          "Content-Type": "application/octet-stream"
+        }
+      })
+      .on("response", function(response) {
+        this.pipe(res);
+      });
+  } catch (error) {
+    res.json({
+      msg: "error",
+      code: 404
+    });
+  }
 });
 
 module.exports = router;
