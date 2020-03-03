b5b44f2b5c0ab71af9e2851ffc645ab50845ac84
candy <htmlcs@163.com>
2020-03-03 22:33:31 +0800

    2020-03-03

--- data/keyword.js
+++ data/keyword.js
@@ -1,14 +1,34 @@
 module.exports = [
   {
     name: "404",
-    xml: "未找到当前关键字对应的数据"
+    xml: "未找到当前关键字对应的数据，回复菜单试试"
   },
   {
     name: "菜单",
-    xml: `1.海豚影视\n一款有温度的影视APP`
+    xml: `关键字列表(请回复中文关键字)\n1.海豚影视\n一款有温度的影视APP\n2.坏坏猫\n免费小说、漫画检索工具\n3.QMD\n付费无损音乐下载器\n4.WPS\nWPS2019专业版永久激活码\n5.人人美剧\n人人美剧App绿化版\n6.数据恢复\n19款专业数据恢复软件`
   },
   {
     name: "海豚影视",
     xml: `海豚影视\n蓝奏网盘 https://www.lanzous.com/i9khmud\n百度网盘 链接：https://pan.baidu.com/s/1OJmT7frC53QIiJewM_5srw\n提取码：ccfz`
+  },
+  {
+    name: "坏坏猫",
+    xml: `坏坏猫\n蓝奏网盘 https://www.lanzous.com/i9n900b\n百度网盘 https://pan.baidu.com/s/14QqOHmZXvnSXuu7_7QzoKg\n提取码：h0sf`
+  },
+  {
+    name: "QMD",
+    xml: `QMD-付费无损音乐下载器\n蓝奏网盘 https://www.lanzous.com/i9oh5id\n百度网盘 https://pan.baidu.com/s/1OA83MtmeQSLbxmxufi6lBw \n提取码：xkg3`
+  },
+  {
+    name: "WPS",
+    xml: `WPS-2019专业版永久激活码(序列号)\n694BF-YUDBG-EAR69-BPRGB-ATQXH\n下载地址：\nhttps://pc.wps.cn/`
+  },
+  {
+    name:"人人美剧",
+    xml:`人人美剧绿化版\n蓝奏网盘： https://www.lanzous.com/i9uh42h\n百度网盘： https://pan.baidu.com/s/1m5O0XKQQDdPzrQdJjq83Jg\n提取码：sak6`
+  },
+  {
+    name:"数据恢复",
+    xml:`数据恢复软件合集\n百度网盘：https://pan.baidu.com/s/1rysbrCY0O0YogyZpd82Qpg\n提取码：jx4f`
   }
 ];
